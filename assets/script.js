76d43641afb88280d99bbfe0ecc1ddb0e605eed1
One Cooking Pot <juneevee.ani@gmail.com>
2022-11-07 16:53:28 +0800

    add final script and css

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,156 @@
+//declare variables
 const CHOICES = ["rock", "paper", "scissors"];
+const DEAD = 0;
+const DMG = 20;
+const MAX_HP = 100;
+const CARDS = document.querySelectorAll('.cards img');
+const PLAYER_CARD = document.querySelector('.you .player-card');
+const COMPUTER_CARD = document.querySelector('.enemy .player-card');
+const PLAYER_SCRATCH = document.querySelector('.you .scratch');
+const COMPUTER_SCRATCH = document.querySelector('.enemy .scratch');
+const RESET_BTN = document.querySelector('#restart-game');
+const CARDS_BOX = document.querySelector('.cards');
+const STAT_H1 = document.querySelector('.round-stat h1');
+const STAT_P = document.querySelector('.round-stat p');
+const MSG_ROUND_WIN = [
+    "You successfully hit Sworn Enemy with a ladle",
+    "You're on the roll",
+    "Sworn enemy's eyes spinning from your explosive fart",
+    "You danced a jig, hey, rig-a-jig-jig",
+    "Who else, you're the best"
+];
+const MSG_ROUND_LOSE = [
+    "Your jaw slightly dislocated from a roundhouse kick",
+    "Come on, you can do better",
+    "You got distracted and Sworn Enemy poked your eye with a pencil",
+    "Sworn Enemy successfully snatched your chicken",
+    "Who says you can't use a katana?"
+];
+const MSG_FINAL_WIN = [ //just two, I can't think of anymore
+    "That's the way to do it!",
+    "How are you so good at this?"
+];
+const MSG_FINAL_LOSE = [
+    "Better luck next time",
+    "Sworn Enemy's laughing in your face"
+];
 
-//array values: tie, win, lose
-let CHOICE_STANDING = [];
+let player_hp = MAX_HP;
+let computer_hp = MAX_HP;
+let player_hp_div = document.querySelector('.you .hp');
+let computer_hp_div = document.querySelector('.enemy .hp');
+let CHOICE_STANDING = []; //array values: tie, win, lose
 CHOICE_STANDING["rock"] = ["rock", "scissors", "paper"];
 CHOICE_STANDING["paper"] = ["paper", "rock", "scissors"];
 CHOICE_STANDING["scissors"] = ["scissors", "paper", "rock"];
 
-//co
+//event listeners
+CARDS.forEach(card => card.addEventListener("click", playGame));
+PLAYER_SCRATCH.addEventListener("animationend", detachAnimation);
+COMPUTER_SCRATCH.addEventListener("animationend", detachAnimation);
+
+PLAYER_CARD.addEventListener("animationend", detachAnimation);
+COMPUTER_CARD.addEventListener("animationend", detachAnimation);
+RESET_BTN.addEventListener("click", restartGame);
+document.querySelector('.table').addEventListener("animationend", function(){
+    document.querySelector('.intro').style.display = "none";
+});
+
+function detachAnimation(el) {
+    this.classList.remove("active");
+}
+
+function restartGame() {
+    player_hp = MAX_HP;
+    computer_hp = MAX_HP;
+
+    player_hp_div.style.width = `${player_hp}%`;
+    player_hp_div.style.backgroundColor = "green";
+
+    computer_hp_div.style.width = `${computer_hp}%`;
+    computer_hp_div.style.backgroundColor = "green";    
+
+    RESET_BTN.style.visibility = "hidden";
+    CARDS_BOX.style.visibility = "visible";
+    PLAYER_CARD.style.visibility = "hidden";
+    COMPUTER_CARD.style.visibility = "hidden";
+    
+    STAT_H1.textContent = "";
+    STAT_P.textContent = "";
+
+}
+
+function playGame(el) {
+        
+    let player_card = this.dataset.card; //returns rock, paper or scissors
+    let computer_card = getComputerChoice();
+
+    let result = playRound(player_card.toLowerCase(), computer_card);
+    let msg = "Tie, dude!";
+    let addition = "I'm rooting on you";
+
+    switch(result) {
+
+        case 0: //tie
+        break;
+
+        case 1: //player won
+            computer_hp = computer_hp - DMG;
+            computer_hp_div.style.width = `${computer_hp}%`;
+            COMPUTER_SCRATCH.classList.add("active");
+
+            msg = "Good, you won!"
+            addition = MSG_ROUND_WIN[Math.floor(Math.random() * 5)];
+            
+            //change color of hp
+            if(computer_hp == 40) { computer_hp_div.style.backgroundColor = "#c33803"; }
+            if(computer_hp == 20) { computer_hp_div.style.backgroundColor = "red"; }
+        break;
+
+        case 2: //player lose
+            player_hp = player_hp - DMG;
+            player_hp_div.style.width = `${player_hp}%`;
+            PLAYER_SCRATCH.classList.add("active");
+
+            msg = "You lose!"
+            addition = MSG_ROUND_LOSE[Math.floor(Math.random() * 5)];
+
+            //change color of hp
+            if(player_hp == 40) { player_hp_div.style.backgroundColor = "#c33803"; }
+            if(player_hp == 20) { player_hp_div.style.backgroundColor = "red"; }            
+        break
+    }
+
+    //update the cards
+    PLAYER_CARD.style.visibility = "visible";
+    PLAYER_CARD.setAttribute("src", `assets/images/${player_card}.png`);
+    PLAYER_CARD.classList.add("active");
+
+    COMPUTER_CARD.style.visibility = "visible";
+    COMPUTER_CARD.setAttribute("src", `assets/images/${computer_card}.png`);
+    COMPUTER_CARD.classList.add("active");
+
+
+    //if someone died, end the game and give option to restart
+    if((player_hp == DEAD) || (computer_hp == DEAD)) {
+
+        //hide the cards and show the restart button
+        RESET_BTN.style.visibility = "visible";
+        CARDS_BOX.style.visibility = "hidden";
+
+        //determine the winner
+        if(player_hp > DEAD) {
+            msg = "Good job!"
+            addition = MSG_FINAL_WIN[Math.floor(Math.random() * 2)];
+        } else {
+            msg = "Too bad!"
+            addition = MSG_FINAL_LOSE[Math.floor(Math.random() * 2)];            
+        }
+
+    }
+    document.querySelector('.round-stat h1').textContent = msg;
+    document.querySelector('.round-stat p').textContent = addition;
+}
 
 function getComputerChoice(){
 
@@ -36,47 +180,3 @@ function playRound(player_card, computer_card) {
     return outcome;
 
 }
-
-function game() {
-
-    const DEAD = 0;
-    let player_hp = 100;
-    let computer_hp = 100;
-
-
-
-    /*for (let ctr = 0; ctr < BESTOF; ctr++) {
-
-        let player_card = prompt("Gimme your best shot. Enter rock, paper or scissors.");
-        let computer_card = getComputerChoice();
-
-        let result = playRound(player_card.toLowerCase(), computer_card);
-        let result_txt = "";
-
-        switch (result) {
-
-            case 0:
-                result_txt = `It's a tie!`;
-            break;
-
-            case 1:
-                result_txt = `You won! ${player_card.toUpperCase()} beats ${computer_card.toUpperCase()}`;
-                player_score++;
-            break;
-
-            case 2:
-                result_txt = `You lose! ${computer_card.toUpperCase()} beats ${player_card.toUpperCase()}`;
-                computer_score++;
-            break;
-
-            default:
-                result_txt = `You entered ${player_card}. Is that even ROCK, PAPER or SCISSORS?`;
-            break;
-        }        
-        console.log(result_txt);
-        console.log(`you:${player_score}   computer:${computer_score}`);
-    }*/
-
-}
-
-//game();
